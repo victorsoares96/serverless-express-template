fa84f59f6c63de5af86a7e58a09529ead7a645d9
Victor Soares <vitorsoares96@hotmail.com>
2023-01-14 20:20:25 -0300

    :adhesive_bandage: fix: fix dist

--- src/middlewares/show-project-version.middleware.ts
+++ src/middlewares/show-project-version.middleware.ts
@@ -1,11 +1,15 @@
 import { NextFunction, Request, Response } from 'express';
-import packageJson from '../../package.json';
+import path from 'path';
+import fs from 'fs';
 
 export default async function showProjectVersion(
   request: Request,
   response: Response,
   next: NextFunction,
 ): Promise<void> {
+  const packageJson = JSON.parse(
+    fs.readFileSync(path.resolve(__dirname, '../../package.json'), 'utf8'),
+  );
   response.header('X-Project-Version', packageJson.version);
   response.header('X-Project-Template-Version', packageJson.templateVersion);
   return next();
